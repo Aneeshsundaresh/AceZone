10ed4ef1220f7cd4d087e0dfe1c7a5a49cb120a5
Aneeshsundaresh <52967787+Aneeshsundaresh@users.noreply.github.com>
2025-03-20 11:09:35 +0530

    Add files via upload

---
+++ GameZone/wwwroot/js/script.js
@@ -0,0 +1,6 @@
+﻿window.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+        const top = section.getBoundingClientRect().top;
+        if (top < window.innerHeight / 1.5) {
+            section.
